142d8dba965660c1680be440a5dc9aa5d121ac74
mickey <shoey25ms@gmail.com>
2023-02-15 10:49:13 -0500

    My answer

--- classes/school.py
+++ classes/school.py
@@ -1,5 +1,6 @@
 from classes.staff import Staff
 from classes.student import Student
+import os, csv
 
 class School:
     def __init__(self, name):
@@ -16,3 +17,26 @@ class School:
         for student in self.students:
             if student.school_id == student_id:
                 return student
+
+    def add_student(self, student_data):
+        student = Student(**student_data)
+        self.students.append(student)
+        self.save()
+
+    def delete_student(self, id):
+        self.students = [s for s in self.students if s.school_id != id]
+        self.save()
+
+    def save(self):
+        # students
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../data/students.csv")
+
+        with open(path, 'w', newline='') as writefile:
+            headers = ['name', 'age', 'role', 'school_id', 'password']
+            writer = csv.DictWriter(writefile, fieldnames=headers)
+            writer.writeheader()
+            for student in self.students:
+                writer.writerow(vars(student))
+
+        return self.students
